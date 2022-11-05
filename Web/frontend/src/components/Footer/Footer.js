b880ae8206203c680b8dc062fae9208193faa90e
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2022-11-05 15:32:44 +0200

    setting react project & cleaning it

---
+++ Web/frontend/src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+  return (
+    <p>Footer</p>
+  );
+}
+ 
+export default Footer;
---
+++ Web/frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+
+const Navbar = () => {
+  return (
+    <p>NavBar</p>
+    );
+}
+ 
+export default Navbar;
+
