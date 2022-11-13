b7aec18efefefd93a28d3ee084267b218b9bb400
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2022-11-13 21:22:26 +0200

    on hover edit

--- Web/frontend/src/components/Footer/Footer.js
+++ Web/frontend/src/components/Footer/Footer.js
@@ -5,6 +5,9 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GoogleIcon from '@mui/icons-material/Google';
 import { Link } from 'react-router-dom';
 import './footStyle.css'
+import { makeStyles } from '@mui/styles';
+
+
 const Footer = () => {
   const date = new Date();
   const laptopsBrands = [{ title: "Apple", path: "/" }, { title: "Dell", path: "/" }, { title: "Acer", path: "/" }, { title: "Lenovo", path: "/" }, { title: "Asus", path: "/" }]
@@ -61,10 +64,10 @@ const Footer = () => {
         <div className="contact">
           <h4 style={{ textAlign: 'center', padding: '10px', alignSelf: "flex-end" }}>Contact us</h4>
           <ul>
-            <li><Link to=""><FacebookIcon color='action' fontSize='large'></FacebookIcon></Link></li>
-            <li><Link to=""><TwitterIcon color='action' fontSize='large'></TwitterIcon></Link></li>
-            <li><Link to=""><InstagramIcon color='action' fontSize='large'></InstagramIcon></Link></li>
-            <li><Link to=""><GoogleIcon color='action' fontSize='large'></GoogleIcon></Link></li>
+            <li><Link to=""><FacebookIcon sx={{"&:hover":{color: "#1976d2"}}}  color='action' fontSize='large'></FacebookIcon></Link></li>
+            <li><Link to=""><TwitterIcon sx={{ "&:hover": { color: "#1976d2" } }} color='action' fontSize='large'></TwitterIcon></Link></li>
+            <li><Link to=""><InstagramIcon sx={{ "&:hover": { color: "#1976d2" } }} color='action' fontSize='large'></InstagramIcon></Link></li>
+            <li><Link to=""><GoogleIcon sx={{ "&:hover": { color: "#1976d2" } }} color='action' fontSize='large'></GoogleIcon></Link></li>
           </ul>
         </div>
       </div>
