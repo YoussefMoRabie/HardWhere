d08abdff1a0decf9a2044a15ac9ff801d0c18383
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2022-12-28 19:12:21 +0200

    security

--- Web/frontend/src/components/utils/PrivateRoute.js
+++ Web/frontend/src/components/utils/PrivateRoute.js
@@ -3,9 +3,14 @@ import { Outlet, Navigate, useLocation } from 'react-router-dom'
 
 const PrivateRoutes = (props) => {
   let {state}=useLocation();
-  console.log(state.auth)
+  const isok=()=>{
+    if (state==null)
+    return false;
+    else
+      return state.auth === 'employee' || state.auth === 'manager'
+  }
   return (
-    state.auth === 'employee' || state.auth ==='manager' ? <Outlet /> : <Navigate to="/signin" />
+     isok()? <Outlet /> : <Navigate to="/signin" />
   )
 }
 
