91d1d9aec6c6b7ba0d790824ccedb3e94cede73b
Mahmoud Sobhy <94849658+MoSobhy01@users.noreply.github.com>
2022-12-29 02:09:26 +0200

    orderpage

--- Web/frontend/src/components/OrderPage/OrderPage.js
+++ Web/frontend/src/components/OrderPage/OrderPage.js
@@ -8,13 +8,17 @@ import Stack from "@mui/material/Stack";
 import { useLocation } from "react-router-dom";
 import React from "react";
 import { color } from "@mui/system";
+import './Order.css'
 
 
 
 
 const OrderPage = () => {
   const { state } = useLocation();
-
+  const [orders, setOrders] = React.useState([{ date: '15 Oct,2022', products: [{}, {}, {}, {}], price: 200 },
+                                              { date: '12 Oct,2022', products: [{}, {}], price: 400 },
+                                              { date: '10 Oct,2022', products: [{}, {}, {}], price: 900 },
+                                              { date: '9 Oct,2022', products: [{}, {}], price: 100 }])
   console.log("user", state);
 
   const { categoryVal: parm } = useParams();
@@ -23,7 +27,7 @@ const OrderPage = () => {
   useEffect(() => {
     const getProducts = async () => {
       try {
-        const dataRes = await fetch(`http://localhost:1444/api/v1/labtops`);
+        const dataRes = await fetch(`http://localhost:1444/api/v1/mobiles`);
         const { data } = await dataRes.json();
         console.log("products", data);
         setProducts(data);
@@ -38,40 +42,45 @@ const OrderPage = () => {
     <div className="proPage">
       <h3>
         {" "}
-        <span>{parm}</span>
+        <span>Your Orders</span>
       </h3>
-      <div className="showproducts">
-        {products.map((product, index) => (
-          <div
-            className="slider-card"
-            key={product.pid}
-          >
+      <div style={{ justifyContent: 'start' }} className="showproducts">
+        <div className="eachorder">
+          <p className="orderDate" >Order at <span style={{ color: '#fc2a5c' }}>12 OCT, 2022</span></p>
+          {products && products.map((product, index) => (
             <div
-              className="slider-card-image"
-              onClick={(e) => {
+              className="ordCard slider-card"
+              key={product.pid}
+            >
+              <div
+                className="slider-card-image"
+                onClick={(e) => {
+                  his(`/product/${product.pid}`, { state: state });
+                }}
+                style={{
+                  backgroundImage: `url(${product.img_link})`,
+                  backgroundSize: "cover",
+                }}
+              ></div>
+              <p onClick={(e) => {
                 his(`/product/${product.pid}`, { state: state });
-              }}
-              style={{
-                backgroundImage: `url(${product.img_link})`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-            <p onClick={(e) => {
-              his(`/product/${product.pid}`, { state: state });
-            }} className="slider-card-title">{product.product_name}</p>
-            <Rating
-              className="slider_rating"
-              name="half-rating-read"
-              defaultValue={product.p_value}
-              precision={0.5}
-              size="small"
-              readOnly
-            />
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <p className="slider-card-price">{product.price}$</p>
+              }} className="slider-card-title">{product.product_name}</p>
+              <Rating
+                className="slider_rating"
+                name="half-rating-read"
+                defaultValue={product.p_value}
+                precision={0.5}
+                size="small"
+                readOnly
+              />
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <p className="slider-card-price">{product.price}$</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+          <p className="orderDate" >Total Cost: <span style={{ color: '#fc2a5c' }}>200$</span></p>
+          <hr />
+        </div>
       </div>
     </div>
   );
